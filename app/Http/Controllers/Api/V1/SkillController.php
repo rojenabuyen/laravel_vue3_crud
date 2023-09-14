1bfa4e0671764998fe57e9ac5dee7771380998bb
Rojen Abuyen <rojenabuyen@gmail.com>
2023-09-14 20:10:01 +0800

    first commit

---
+++ app/Http/Controllers/Api/V1/SkillController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreSkillRequest;
+use App\Http\Resources\V1\SkillCollection;
+use App\Http\Resources\V1\SkillResource;
+use App\Models\Skill;
+use Illuminate\Http\Request;
+
+class SkillController extends Controller
+{
+    public function index()
+    {
+        return new SkillCollection(Skill::all());
+    }
+
+    public function show(Skill $skill)
+    {
+        return new SkillResource($skill);
+    }
+
+
+    public function store(StoreSkillRequest $request)
+    {
+        Skill::create($request->validated());
+
+        return response()->json("Skill created");
+    }
+
+    public function update(StoreSkillRequest $request, Skill $skill)
+    {
+        $skill->update($request->validated());
+
+        return response()->json("Skill updated");
+    }
+
+    public function destroy(Skill $skill)
+    {
+        $skill->delete();
+
+        return response()->json("Skill deleted.");
+    }
+}
